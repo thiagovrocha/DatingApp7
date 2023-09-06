a8456ca6994fbda3c6b5b22ddaa74e01792af43c
thiagorocha <Thiago.Rocha.T0121535@edu.atec.pt>
2023-09-06 22:23:09 +0100

    End of Section 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext : DbContext //Faz herança da Classe DBContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<AppUser> Users { get; set; }
+}
