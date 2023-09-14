2cebb006341ba4a40bfe37deac1d6a275c50b9c5
thiagorocha <Thiago.Rocha.T0121535@edu.atec.pt>
2023-09-14 23:34:53 +0100

    Version 3

--- API/Program.cs
+++ API/Program.cs
@@ -12,12 +12,17 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+//Add CORS to authorize Client IP to get info from APO
+builder.Services.AddCors();
+
 var app = builder.Build();
 
+//Middlewares Area
 // Configure the HTTP request pipeline.
-app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//Allowing Headers and Methods from the client side (localhost:4200)
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
 
